5597664e86a4f315ae12ccbbda92d79c96f91ccc
john van strien <john2525@earthlink.net>
2016-10-08 01:12:34 -0700

    Changed extension.

---
+++ Colors.java
@@ -0,0 +1,29 @@
+
+
+// static class Colors {
+class Colors {
+
+  public static final int HUE_MIN = 0;
+  public static final int HUE_MAX = 100;
+  public static final int HUE_HALF = 127;
+
+  public static final int SAT_MIN = 0;
+  public static final int SAT_MAX = 100;
+
+  public static final int BRIGHT_MIN = 0;
+  public static final int BRIGHT_MAX = 100;
+
+  // public static final color RED   = 0xFFFF0000;
+  // public static final color GREEN = 0xFF00FF00;
+  // public static final color BLUE  = 0xFF0000FF;
+  // public static final color BLACK = 0xFF000000;
+  // public static final color WHITE = 0xFFFFFFFF;
+
+  public static final int RED   = 0xFFFF0000;
+  public static final int GREEN = 0xFF00FF00;
+  public static final int BLUE  = 0xFF0000FF;
+  public static final int BLACK = 0xFF000000;
+  public static final int WHITE = 0xFFFFFFFF;
+
+  public static final int WHITE_GRAYSCALE = 255;
+}
