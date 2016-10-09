5fc8ed2e7f3164a54acb82fa43e78b898cf9f599
john van strien <john2525@earthlink.net>
2016-10-08 19:15:32 -0700

    Corrected the value of HUE_HALF. Added white and black gray scale values.

--- Colors.java
+++ Colors.java
@@ -5,13 +5,16 @@ class Colors {
 
   public static final int HUE_MIN = 0;
   public static final int HUE_MAX = 100;
-  public static final int HUE_HALF = 127;
+  public static final int HUE_HALF = HUE_MAX / 2;
 
   public static final int SAT_MIN = 0;
   public static final int SAT_MAX = 100;
 
   public static final int BRIGHT_MIN = 0;
   public static final int BRIGHT_MAX = 100;
+
+  public static final int BLACK_GREY_SCL = 0;
+  public static final int WHITE_GREY_SCL = 100;
 
   // public static final color RED   = 0xFFFF0000;
   // public static final color GREEN = 0xFF00FF00;
@@ -24,6 +27,4 @@ class Colors {
   public static final int BLUE  = 0xFF0000FF;
   public static final int BLACK = 0xFF000000;
   public static final int WHITE = 0xFFFFFFFF;
-
-  public static final int WHITE_GRAYSCALE = 255;
 }
