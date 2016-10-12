2d88ffb13e1b611bebeed7a5ecd4f0c72d92d982
john van strien <john2525@earthlink.net>
2016-10-12 00:10:35 -0700

    Changed class modifier. Added constants for several hue values. Commented out he RGB constants.

--- Colors.java
+++ Colors.java
@@ -1,11 +1,17 @@
+public final class Colors {
 
-
-// static class Colors {
-class Colors {
-
+  // HSB support
   public static final int HUE_MIN = 0;
   public static final int HUE_MAX = 100;
-  public static final int HUE_HALF = HUE_MAX / 2;
+  public static final int HUE_HALF = 50;
+
+  public static final int HUE_RED = 0;
+  public static final int HUE_ORANGE = 10;
+  public static final int HUE_YELLOW = 15;
+  public static final int HUE_GREEN = 33;
+  public static final int HUE_CYAN = 50;
+  public static final int HUE_BLUE = 65;
+  public static final int HUE_MAGENTA = 82;
 
   public static final int SAT_MIN = 0;
   public static final int SAT_MAX = 100;
@@ -16,15 +22,21 @@ class Colors {
   public static final int BLACK_GREY_SCL = 0;
   public static final int WHITE_GREY_SCL = 100;
 
-  // public static final color RED   = 0xFFFF0000;
-  // public static final color GREEN = 0xFF00FF00;
-  // public static final color BLUE  = 0xFF0000FF;
-  // public static final color BLACK = 0xFF000000;
-  // public static final color WHITE = 0xFFFFFFFF;
 
-  public static final int RED   = 0xFFFF0000;
-  public static final int GREEN = 0xFF00FF00;
-  public static final int BLUE  = 0xFF0000FF;
-  public static final int BLACK = 0xFF000000;
-  public static final int WHITE = 0xFFFFFFFF;
+    // RGB support
+  // public final color RED  (0xFFFF0000;
+  // public static final color GREEN(0xFF00FF00;
+  // public static final color BLUE (0xFF0000FF;
+  // public static final color BLACK(0xFF000000;
+  // public static final color WHITE(0xFFFFFFFF;
+
+  // public static final int RED  (0xFFFF0000;
+  // public static final int GREEN(0xFF00FF00;
+  // public static final int BLUE (0xFF0000FF;
+  // public static final int BLACK(0xFF000000;
+  // public static final int WHITE = 0xFFFFFFFF;
+
+  private Colors() {
+  }
+
 }
